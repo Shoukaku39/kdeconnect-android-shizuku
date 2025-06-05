9e740e953b97387fadf32a6f14e00bfb4f6e4020
Akagi <Shoukaku61@proton.me>
2025-06-05 19:01:45 +0800

    添加了一些东西

--- src/org/kde/kdeconnect/Plugins/ClibpoardPlugin/ClipboardListener.java
+++ src/org/kde/kdeconnect/Plugins/ClibpoardPlugin/ClipboardListener.java
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2014 Albert Vaca Cintora <albertvaka@gmail.com>
+ * SPDX-FileCopyrightText: 2021 Ilmaz Gumerov <ilmaz1309@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
+
 package org.kde.kdeconnect.Plugins.ClibpoardPlugin;
 
 import android.Manifest;
